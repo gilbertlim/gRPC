ff0b0a6c1363aeeeb3c835afb655764faac63488
gilbertlim <9ilbert.app@gmail.com>
2023-06-21 18:43:31 +0900

    fix spring boot version compatibility

--- spring-boot/grpc-server/src/main/java/com/gilbert/grpc/service/SampleService.java
+++ spring-boot/grpc-server/src/main/java/com/gilbert/grpc/service/SampleService.java
@@ -4,12 +4,19 @@ import com.gilbert.grpc.proto.SampleRequest;
 import com.gilbert.grpc.proto.SampleResponse;
 import com.gilbert.grpc.proto.SampleServiceGrpc;
 import io.grpc.stub.StreamObserver;
+import javax.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
+import net.devh.boot.grpc.server.service.GrpcService;
 
 @Slf4j
-@Service
-public class SampleServiceImpl extends SampleServiceGrpc.SampleServiceImplBase {
+//@Service
+@GrpcService
+public class SampleService extends SampleServiceGrpc.SampleServiceImplBase {
+    
+    @PostConstruct
+    public void test() {
+        System.out.println("log = " + log);
+    }
 
     @Override
     public void sampleCall(SampleRequest request, StreamObserver<SampleResponse> responseObserver) {
